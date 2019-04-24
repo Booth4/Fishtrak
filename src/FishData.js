ac27b76932c7e3b4480c8f52dfb5efedb07e4705
Kyle Booth <kylebooth4@gmail.com>
2019-04-24 10:20:08 -0400

    table sorting

--- src/FishData.js
+++ src/FishData.js
@@ -20,7 +20,11 @@ class FishData extends Component {
 					id: fishItem,
 					user: fish[fishItem].user,
 					length: fish[fishItem].length,
-					weight: fish[fishItem].weight
+					weight: fish[fishItem].weight,
+					lineLength: fish[fishItem].lineLength,
+					sinkerWeight: fish[fishItem].sinkerWeight,
+					lure: fish[fishItem].lure,
+					color: fish[fishItem].color,
 				});
 			}
 			this.setState({
@@ -29,22 +33,58 @@ class FishData extends Component {
 		});
 	}
 
+	sort(key) {
+		var newFish = this.state.fish;
+		
+		newFish.sort(function(a, b) {
+			return a[key] - b[key];
+		});
+		
+		/*newFish.sort(function(a, b){
+			alert("a: " + a[key] + ", b: " + b[key]);
+			var compA = a[key];
+			var	compB = b[key];
+			
+			if(compA < compB){
+				return -1;
+			} 
+			if(compA > compB){
+				return 1;
+			} 
+			return 0;
+		});*/
+
+		this.setState({
+			fish: newFish
+		});
+	}
+
 	render() {
 		return (
 			<div className="">
-	  	  <table>
-					<tbody>
+	  	  <table className="table">
+					<thead>
 					<tr>
-						<th>Fisherman</th>
-						<th>Length</th>
-						<th>Weight</th>
+						<th onClick={() => this.sort('user')}>Fisherman</th>
+						<th onClick={() => this.sort('length')}>Length</th>
+						<th onClick={() => this.sort('weight')}>Weight</th> 
+						<th onClick={() => this.sort('lineLength')}>Line Length</th> 
+						<th onClick={() => this.sort('sinkerWeight')}>Sinker Weight</th> 
+						<th onClick={() => this.sort('lure')}>Lure</th> 
+						<th onClick={() => this.sort('color')}>Lure Color</th> 
 					</tr>
+					</thead>
+					<tbody>
   		    {this.state.fish.map((fishItem) => {
   		      return (
 							<tr key={fishItem.id}>
 								<td>{fishItem.user}</td>
 								<td>{fishItem.length}</td>
   		          <td>{fishItem.weight}</td>
+  		          <td>{fishItem.lineLength}</td>
+  		          <td>{fishItem.sinkerWeight}</td>
+  		          <td>{fishItem.lure}</td>
+  		          <td>{fishItem.color}</td>
 							</tr>
   		          
   		      )
